df322d0ea2e6b19bfa5d3b912ce26f457a3de4f9
chiu <103289149+Linqqqqqq@users.noreply.github.com>
2026-03-13 12:39:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<script>
+const button = document.getElementById("escapeButton");
+
+button.addEventListener("mouseover", () => {
+
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  button.style.left = randomX + "px";
+  button.style.top = randomY + "px";
+
+});
+</script>
